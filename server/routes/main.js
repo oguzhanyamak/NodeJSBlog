52e177e0c510378dc6f0007d08feecd5781fc1d1
Oğuzhan YAMAK <oguzhanyamak@outlook.com>
2025-02-08 21:38:43 +0300

    MongoDB Bağlantısı ve HomePage düzenlemesi

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,18 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
 
+router.get("", async (req, res) => {
+  const locals = {
+    title: "Node Blog",
+    description: "lorem ipsum dolor sit amet",
+  };
 
-router.get('',(req,res) => {
-    const locals = {
-        title:"Node Blog",
-        description : "lorem ipsum dolor sit amet"
-    }
-    res.render('index',{locals});
+  try {
+    let perPage = 5;
+    let page = req.query.page || 1;
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.get('/about',(req,res) => {
-    res.render('about')
-})
-
+router.get("/about", (req, res) => {
+  res.render("about");
+});
 
 module.exports = router;
