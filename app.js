05620f7e93b7851feb5b0246856e8633d1a19111
Oğuzhan YAMAK <oguzhanyamak@outlook.com>
2025-02-09 22:03:50 +0300

    search functionality

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const PORT = 5000 || process.env.PORT;
 connectDB();
 
 app.use(express.static('public'));
-
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 app.use(expressLayout);
 app.set('layout','./layouts/main');
--- server/routes/main.js
+++ server/routes/main.js
@@ -33,4 +33,43 @@ router.get("/about", (req, res) => {
   res.render("about");
 });
 
+router.get("/post/:id", async (req, res) => {
+  try {
+    let slug = req.params.id;
+    const data = await Post.findById({ _id: slug });
+    const locals = {
+      title: data.title,
+      description: "Sample Blog NodeJs & Mongo",
+    };
+    res.render("post", { data, locals });
+  } catch (error) {}
+});
+
+router.post("/search", async (req, res) => {
+  try {
+    const locals = {
+      title: "Seach",
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
+
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+      ],
+    });
+
+    res.render("search", {
+      data,
+      locals,
+      currentRoute: "/",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;
