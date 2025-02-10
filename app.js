207caf0c67e420b3349397391c61adfbdf4de8eb
Oğuzhan YAMAK <oguzhanyamak@outlook.com>
2025-02-10 18:40:38 +0300

    Login,Register,Auth Infras....

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const expressLayout = require('express-ejs-layouts');
 const express = require('express');
 const connectDB = require('./server/config/db');
-
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
 
 
 const app = express();
@@ -13,6 +15,15 @@ connectDB();
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:true,
+    store:MongoStore.create({
+        mongoUrl:process.env.MONGODB_URI
+    })
+}))
 
 app.use(expressLayout);
 app.set('layout','./layouts/main');
@@ -21,6 +32,8 @@ app.set('view engine','ejs');
 
 
 app.use('/',require('./server/routes/main'));
+app.use('/',require('./server/routes/admin'));
+
 
 app.listen(PORT, () => {
     console.log(`App Listening on : ${PORT}`)
